946c547cbef7ee59085a735f07bb732e574f786c
Thibault Clérice <leponteineptique@gmail.com>
2014-10-22 19:03:48 +0200

    Sample system and new architecture

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import xml.etree.cElementTree as cElementTree
 import xml.etree.ElementTree as ElementTree
 import re
 from xml.dom import minidom
+import copy
 
 
 def prettify(elem):
@@ -142,9 +143,10 @@ def divideText(text, index):
 ol_match = re.compile("^([1-9]{1,3}|[abcdefABCDEF]{1}|IX|IV|V?I{0,3})$")
 
 i = 0
-limit = 90000
+limit = 100 #For the sample
+break_on_sample = False
 #Corrected = 400
-with open("Georges_1913_no_header.xml") as f:
+with open("input/Georges_1913_no_header.xml") as f:
 
 	root = cElementTree.Element("div")
 	#In this document, we have one line = one word definition, h1 represent orth
@@ -207,44 +209,15 @@ with open("Georges_1913_no_header.xml") as f:
 				else:
 					id = text
 
-			
-		"""
-		<H1>A. 1. A, a,</H1>
-		<I>der erste Buchstabe des lateinischen Alphabets. – Als Abkürzung:</I>
-		1) = der Vorname Aulus.  
-		<p>
-		2) = Antiquo <I>(ich verwerfe den neuen Vorschlag), auf den Stimmtafeln in röm. Volksversammlungen.</I> – 
-		3) = Absolvo <I>(ich spreche frei), auf den Stimmtafeln der Richter; dah.</I> 
-		A gen. littera salutaris<I> bei</I> Cic. Mil. 15. – 
-		4) <I>vor Zahlen, Jahresbezeichnung</I> (= Annus) <I>auf Grabschriften usw.: u. so </I><B>A. U. C.</B> = anno urbis conditae – <I>aber </I><B>a. u. c.</B> = ab urbe condita. – <I>u. </I><B>a. d. </B>= ante diem <I>als Datum.</I> – 
-		5) = Augustus, <I>häufig in Inschriften; </I><B>A. A.</B> = Augusti duo;<B> A. A. A.</B> = Augusti tres. – <I>aber </I><B>A. A. A. F. F. </B><I>nach</I> III viri = auro, argento, aeri flando, feriundo. – 
-		6) = Auditor, <I>im Gegensatz zu</I> M(agister) <I>in</I> Cic. Tusc. disp. 
-		</p>
-
-		OR 
-		<H1>abāctio,</H1> 
-		ōnis, f. (abigo) = εξέλασις, <I>das </I><B><I>Wegtreiben,</I></B> Hier. in Ierem. 1, 5, 15. Gloss. II, 302, 50. 
-		"""
-
-		"""
-		<entryFree id="n10" type="hapax" key="abaculus">
-			<orth extent="full" lang="la">&abreve;b&abreve;c&ubreve;lus</orth>
-			, <itype>i</itype>, <gen>m.</gen> 
-			<lbl>dim.</lbl>
-			<etym>abacus</etym>, 
-			<sense id="n10.0" n="I" level="1">
-				<hi rend="ital">a small cube or tile of colored glass for making ornamental pavements</hi>,
-				 the Gr. <foreign lang="greek">u\buki/skos</foreign>, 
-				<bibl n="urn:cts:latinLit:phi0978.phi001:36:67"><author>Plin.</author> 36, 26, 67, &sect; 199</bibl>. <cb n="ABAL"/>
-			</sense>
-		</entryFree>
-		"""
 		if i == limit:
-			break
+			with open("output/sample.xml", "w") as f:
+				f.write(prettify(copy.deepcopy(root)))
+				f.close()
+			if break_on_sample:
+				break
 		i += 1 
 
-tree = cElementTree.ElementTree(root)
-
-with open("here.xml", "w") as f:
-	f.write(prettify(root))
-	f.close()
+if not break_on_sample:
+	with open("output/output.xml", "w") as f:
+		f.write(prettify(root))
+		f.close()
